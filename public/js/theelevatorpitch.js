2d11535a752a3bb1b8c0f1b765c2270c01d811e7
ymmtmsys <m2yamamoto@gmail.com>
2012-11-14 08:16:55 +0900

    scroll

--- public/js/theelevatorpitch.js
+++ public/js/theelevatorpitch.js
@@ -1,7 +1,15 @@
 $(function() {
     $(".btn-start").click(function () {
-        var p = $(".section-form").offset().top;
-        $('html,body').animate({ scrollTop: p }, 'fast');
-        return false;
+        return scrollDown($(".section-form"));
     });
+
+    $(".btn-try").click(function () {
+        return scrollDown($(".section-try"));
+    });
+
+    function scrollDown(obj) {
+        var top = obj.offset().top;
+        $('html,body').animate({ scrollTop: top }, 'fast');
+        return false;
+    }
 });
